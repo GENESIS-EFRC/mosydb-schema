7cbbc62bb04163c779662083212e7dca5cd3faa9
raidun99 <raidun99@users.noreply.github.com>
2021-04-15 23:04:08 +0000

    deploy: 37b66aa413130e3a2e4ac4cb260ad3fdb85d0d4b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g3afea61',
+    VERSION: '0.post1+g37b66aa',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
