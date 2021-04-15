58ee1ba32e40f46179ec55e59991da063a4816fe
raidun99 <raidun99@users.noreply.github.com>
2021-04-15 01:17:19 +0000

    deploy: 574103a948905556ede89347641ee637e8d6f634

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+gf964c06',
+    VERSION: '0.post1+g574103a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
